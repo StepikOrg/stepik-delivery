1e40c823d1fdc127a81acab822224efa8a68053f
Egor Dubchenko <dubchenkoegor@gmail.com>
2019-05-12 01:25:07 +0300

    fix

--- src/services/Api.js
+++ src/services/Api.js
@@ -3,9 +3,7 @@ import axios from 'axios'
 export default () => {
   const instance = axios.create({
 
-    baseURL: `${localStorage.getItem('apiIp')}${process.env.NODE_ENV === 'production'
-      ? 'stepik-delivery/'
-      : ''}`,
+    baseURL: `${localStorage.getItem('apiIp')}`,
     headers: {
       'Content-Type': 'application/json'
     }
